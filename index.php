bbc7bb67c17ab2cc7ed4f03f95596ce92a57059b
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-10-08 20:04:56 -0300

    configurando rotas

--- index.php
+++ index.php
@@ -9,7 +9,54 @@
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
+    <nav class="navbar bg-body-tertiary fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Cadastro</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Cadastro</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="?page=novo">Novo usuário</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="?page=listar">Listar usuários</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col mt-5">
+                <?php
+                switch (@$_REQUEST["page"]) {
+                    case 'novo':
+                        include("novo-usuario.php");
+                        break;
+                    case 'listar':
+                        include("listar-usuario.php");
+                        break;
+                    default:
+                        print "<h1>Bem vindos!</h1>";
+                        break;
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
