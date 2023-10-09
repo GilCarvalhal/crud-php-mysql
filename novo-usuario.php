42be969037718516e503431f2aeda4bfb5a0a539
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-10-09 15:17:47 -0300

    criando o form na pagina de novos usuarios

--- novo-usuario.php
+++ novo-usuario.php
@@ -1,1 +1,26 @@
 <h1>Novo Usuário</h1>
+<form action="?page=salvar" method="post">
+    <div class="mb-3">
+        <label>Nome</label>
+        <input type="text" name="nome" class="form-control">
+    </div>
+
+    <div class="mb-3">
+        <label>Email</label>
+        <input type="email" name="email" class="form-control">
+    </div>
+
+    <div class="mb-3">
+        <label>Senha</label>
+        <input type="password" name="senha" class="form-control">
+    </div>
+
+    <div class="mb-3">
+        <label>Data de nascimento</label>
+        <input type="date" name="data_nasc" class="form-control">
+    </div>
+
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+</form>
