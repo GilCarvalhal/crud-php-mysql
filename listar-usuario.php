e0440d3295f5222619377d17560817a93c8f3bc9
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-10-10 14:45:47 -0300

    crud - read

--- listar-usuario.php
+++ listar-usuario.php
@@ -1,1 +1,30 @@
 <h1>Listar Usuários</h1>
+
+<?php
+
+$sql = "SELECT * FROM usuarios";
+
+$res = $conn->query($sql);
+
+$qtd = $res->num_rows;
+
+if ($qtd > 0) {
+    print "<table class='table table-hover table-striped table-bordered'>";
+    while ($row = $res->fetch_object()) {
+        print "<tr>";
+        print "<th>#</th>";
+        print "<th>Nome</th>";
+        print "<th>E-mail</th>";
+        print "<th>Data de Nascimento</th>";
+        print "</tr>";
+        print "<tr>";
+        print "<td>" . $row->id . "</td>";
+        print "<td>" . $row->nome . "</td>";
+        print "<td>" . $row->email . "</td>";
+        print "<td>" . $row->data_nasc . "</td>";
+        print "</tr>";
+    }
+    print "</table>";
+} else {
+    print "<p class='alert alert-danger'>Não encontrou resultados!</p>";
+}
